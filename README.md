16b8eef60bd395b90cc7d275557ba9fc53d5df6a
Taylor Brooks <tybrooks01@icloud.com>
2023-01-17 08:53:48 -0500

    sunrise app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# sunrise-app
-A simple application to view the sunrise, sunset &amp; moon phase in your area
+Sunrise Application 
+
+    A simple application to view the sunrise, sunset & moon phase in your area.
+
+- User inputs location & app returns :
+    - Sunrise Time
+    - Sunset Time
+    - Current Moon Phase
+If user inputted area cannot be found, an error message will display. 
+
---
+++ file.js
@@ -0,0 +1,68 @@
+const BASE_URL = 'https://wttr.in'
+const form = document.querySelector("form")
+const userSearch = document.querySelector(".userSearch")
+const sunrise = document.querySelector("#sunrise")
+const sunset = document.querySelector("#sunset")
+const moonInfo = document.querySelector("#moon-info")
+const main = document.querySelector("main")
+
+// api organization
+form.addEventListener("submit", getApiData)
+
+
+function getApiData(event){
+    event.preventDefault()
+    const location =`${userSearch.value}`
+    const urlFormat = `${BASE_URL}/${location}?format=j1`
+    getAstroInfo(urlFormat,location)
+    form.reset()
+    } 
+
+    function getAstroInfo(url,location){
+    fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+        getSunrise(result);
+        getSunset(result);
+        getMoonPhase(result);
+    })
+    .catch((error) => {
+        console.log("Location not found");
+
+        errorMessage(error)
+    })
+    }
+function errorMessage(){
+    console.log("Location not found")
+ };
+
+
+
+// functions
+
+
+
+
+function getSunrise(result){
+    const sunriseP = document.createElement("p")
+    sunriseP.innerHTML = `<p> <strong> Sunrise Time : </strong> ${result.weather[0].astronomy[0].sunrise} </p>`
+    sunrise.append(sunriseP)
+}
+
+
+function getSunset(result){
+    const sunsetP = document.createElement("p")
+    sunsetP.innerHTML = `<p> <strong> Sunset Time : </strong> ${result.weather[0].astronomy[0].sunset} </p>`
+    sunset.append(sunsetP)
+}
+
+
+function getMoonPhase(result){
+    const moonP = document.createElement("p")
+    moonP.innerHTML = `<p> <strong> Moon Phase : </strong> ${result.weather[0].astronomy[0].moon_phase} </p>`
+    moonInfo.append(moonP)
+}
+
+
+
+
